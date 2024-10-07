f0377334d0635afa38f5b0882cb0320d82424a71
Kaiwen Li <kaiwen.li21@imperial.ac.uk>
2024-10-07 12:09:26 +0100

    Changed gitignore, now it won't add sandbox

---
+++ Week1/Code/UnixPrac1.sh
@@ -0,0 +1,7 @@
+#count how many lines there are in each file
+#print everything starting from the second line for the E. coli genome
+#count the sequence length of this genome
+#count the matches of a particular sequence "ATGC" in the genome of E. coli
+#Compute the A+T/G+C ratio
+
+grep .fasta -c
---
+++ Week1/code/boilerplate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Author: Kaiwen Li kl2621@imperial.ac.uk
+# Script: boilerplate.sh
+# Desc: simple boilerplate for shell scripts
+# Arguments: none
+# Date: Oct 2019
+
+echo -e "\nThis is a shell script! \n"
+
+#exit
