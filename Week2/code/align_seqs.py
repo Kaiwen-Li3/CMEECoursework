91a42170962a4ac41a6e95c06b68c280d1aa2e2a
Kaiwen Li <kaiwen.li21@imperial.ac.uk>
2024-10-18 14:23:46 +0100

    small edits to align_seqs

--- Week2/code/align_seqs.py
+++ Week2/code/align_seqs.py
@@ -3,6 +3,7 @@ import csv
 import sys
 
 #==================================================================================================
+#orders sequences
 
 def orderseqs(seq1,seq2):
     l1 = len(seq1)
@@ -14,8 +15,6 @@ def orderseqs(seq1,seq2):
         s1 = seq2
         s2 = seq1
     l1, l2 = l2, l1 # swap the two lengths
-
-
 
     return s1,s2,l1,l2
 
@@ -36,16 +35,16 @@ def calculate_score(s1, s2, l1, l2, startpoint):
     import ipdb; ipdb.set_trace()
 
     # some formatted output
-    aligned_matched = ("." * startpoint + matched)           
+    aligned_matched = ("." * startpoint + matched)     #first prints 'startpoint' amount of dots      
     aligned_s2 = ("." * startpoint + s2)
     aligned_s1 = (s1)
     print(score) 
     print(" ")
     
-
     return aligned_matched, aligned_s2, aligned_s1, score
 
 #==================================================================================================
+#main
 
 def main(argv): 
     seqs = []
@@ -53,10 +52,11 @@ def main(argv):
     g = open('../results/align_seqs_result.csv','w',newline='')
     seqs = csv.reader(f)
     csvwrite = csv.writer(g)
-    next(seqs)
+    next(seqs) #skips header line
 
-    for row in seqs:
-        rowordered = (orderseqs(row[0],row[1]))
+    for row in seqs: #for each row in _data.csv
+
+        rowordered = (orderseqs(row[0],row[1])) #runs orderseqs on the current row
 
         #set up best variables for match, alignment, score 
         best_matched = None
@@ -64,23 +64,20 @@ def main(argv):
         best_score = -1
         
 
-
-
-        for i in range(rowordered[2]):
-            z = calculate_score(rowordered[0],rowordered[1],rowordered[2],rowordered[3],i)
-            if z[3] > best_score:
+        for i in range(rowordered[2]): #tries all startpoints
+            z = calculate_score(rowordered[0],rowordered[1],rowordered[2],rowordered[3],i) #runs calculate_score on the current startpoint
+            if z[3] > best_score: #if the current startpoint surpasses the best score, make it the new best
                 best_matched = z[0]
                 best_align = "." * i + rowordered[1]
                 best_score = z[3]
 
             #import ipdb; ipdb.set_trace()
-                
-                
+                        
 
         final_result = (best_matched, best_align, rowordered[0], best_score)
 
         print("The highest score alignment is: ")
-        for entry in final_result:
+        for entry in final_result: #writes out each entry in final_result, line by line
             csvwrite.writerow([entry])
             print(entry)
 
