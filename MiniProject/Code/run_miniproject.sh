bf25419fa75cee642bd79be4a3448f8b790bcc3f
Kaiwen Li <kaiwen.li21@imperial.ac.uk>
2025-03-14 07:32:05 +0000

    Touched up the .sh script for the miniproject

--- MiniProject/Code/run_miniproject.sh
+++ MiniProject/Code/run_miniproject.sh
@@ -16,6 +16,19 @@ set -e
 data_prep="DataPreparation.R"
 model_fitter_analysis="Model_Fitting_Analysis.R"
 
+
+install_r_packages() {
+    Rscript -e 'packages <- c("ggplot2", "minpack.lm");
+                missing <- packages[!(packages %in% installed.packages()[,"Package"])];
+                if(length(missing)) install.packages(missing, repos="https://cloud.r-project.org/")'
+}
+
+# Install required R packages if not installed
+echo "Checking and installing required R packages..."
+install_r_packages
+
+
+
 # Data preparation
 echo "Preparing data:"
 Rscript "$data_prep"
